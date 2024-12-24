24b15079784cee9a0f847346b7696151e41f38a6
alrk14567 <cdutik@gmail.com>
2024-12-24 15:38:37 +0900

    [level 0] Title: 5명씩, Time: 0.02 ms, Memory: 84.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181886. 5명씩/5명씩.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String[] solution(String[] names) {
+        int len=0;
+        if(names.length%5==0){
+            len=names.length/5;            
+        }else {
+            len=names.length/5+1;
+        }
+        String[] answer = new String[len];
+        
+        for(int i=0;i<answer.length;i++){
+            answer[i]=names[i*5];
+        }
+        return answer;
+    }
+}
